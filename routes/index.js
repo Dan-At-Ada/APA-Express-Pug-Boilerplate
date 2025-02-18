968d0592e1ac008f0163857d4c5511c5c9be9c18
Dan <daniel@ada.ac.uk>
2025-02-18 10:40:44 +0000

    Update More Routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ const router = express.Router();
 
 // Home route
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express Pug Boilerplate' });
+  res.render('index', { title: 'This is my new website', foods: [] });
 });
+
+router.get('/count/:id', (req, res) => {
+  console.log(req.params.id)
+  if (req.params.id > 5) {
+    res.render('index', { title: "more than 5", foods: [] })
+  }
+  res.render('index', { title: req.params.id, foods: [] })
+})
+
+router.get('/list', (req,res) => {
+  console.log("List Web")
+  const items = [
+    {name: "Pizza"}, 
+    {name: "Pasta"}, 
+    {name: "Noodles"}, 
+    {name: "KFC"}, 
+    {name: "Sandwich"}
+  ]
+  res.render('index', {title: "This is a List", foods: items })
+})
+
 
 module.exports = router;
